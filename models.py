75fec7b96a0dc82c842ae3dc2a30ae06f775ad78
Pedro Santos <72759281+PHenrique07@users.noreply.github.com>
2026-03-10 16:42:15 -0300

    feat: models de usuario e funcoes CRUD de cadastro e busca

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, Field
+
+# 1. Tabela de Usuário 
+class Usuario(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    nome: str
+    email: str = Field(unique=True, index=True) # Impede e-mail repetido
+    senha: str
+    xp: int = Field(default=0)
+    idade: int
+    tipo_usuario: str = Field(default="aluno")
